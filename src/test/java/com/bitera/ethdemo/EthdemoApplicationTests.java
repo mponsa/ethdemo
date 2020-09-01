fbddd74cbbe14c6fbb22baf08b1710a7e9a41ba6
mponsa-meli <manuel.ponsa@mercadolibre.com>
2020-09-01 08:20:29 -0300

    First commit

---
+++ src/test/java/com/bitera/ethdemo/EthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitera.ethdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
